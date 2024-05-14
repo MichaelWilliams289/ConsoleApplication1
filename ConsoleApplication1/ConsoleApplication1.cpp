6949d701fdbdcdb1243475a4f3374d31631ed6ac
gamer <gamer@DESKTOP-IGEVES7>
2024-05-13 19:26:28 -0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+	int luckly_number = 10;
+	cout << "What is your luckly number" << endl;
+	cin >> luckly_number;
+	cout << "your number is " << luckly_number;
+
+}
+
+
